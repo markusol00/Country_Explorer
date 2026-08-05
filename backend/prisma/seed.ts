abb5b7d3f840a70952c7f64d225fe1bff4115cda
Markus Olsen <markusol@hiof.no>
2026-08-05 20:05:54 +0200

    Updated logic in file Seed.ts
    Added the type RawCountry.
    imported readFile for reading JSON file for country-data.
    Added logic for file reading JSON

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,16 +1,58 @@
 import { prisma } from "../src/lib/prisma";
+import { readFile } from "node:fs/promises"
+
+type RawCountry = {
+    cca2: string;
+    name: {
+        common: string;
+    };
+    capital?: string[];
+    currencies?: Record<
+    string,
+    {
+      name: string;
+      symbol?: string;
+    }
+  >;
+  flag?: string;
+  region: string;
+}
 
 async function main() {
-    const response = await fetch(
-        "https://restcountries.com/v3.1/all?fields=cca2,name,capital,population,currencies,flags,region"
-    );
-    if(!response.ok){
-        throw new Error(
-            `Failed to fetch countries. Status: ${response.status} ${response.statusText}`
-        )
-    }
-    const countries = await response.json();
+    const filePath = new URL("./data/countries.json", import.meta.url);
 
+    const fileContent = await readFile(filePath, "utf-8");
+
+    const countries: RawCountry[] = JSON.parse(fileContent);
+
+    const firstCountry = countries[0];
+
+    if(!firstCountry){
+        throw new Error("No countries found in the JSON file");
+    }
+
+    const countriesToSeed = countries.map((country) => ({
+        code: firstCountry.cca2,
+        name: firstCountry.name.common,
+        capital: firstCountry.capital?.[0] ?? null,
+        population: 0, 
+        currency: Object.keys(firstCountry.currencies ?? {})[0] ?? null,
+        flag: firstCountry.flag ?? null,
+        continent: firstCountry.region,
+    }));
+
+
+/*
+    for(let country of countries){
+        await prisma.country.create({
+            data: country
+            cou
+
+        })
+
+    }
+   
+*/
 }
 
 main(); 
