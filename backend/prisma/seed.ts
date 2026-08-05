a396ae59147b78097b5be951b40fbe7061442092
Markus Olsen <markusol@hiof.no>
2026-08-05 15:12:19 +0200

    Error handling

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -4,6 +4,13 @@ async function main() {
     const response = await fetch(
         "https://restcountries.com/v3.1/all?fields=cca2,name,capital,population,currencies,flags,region"
     );
+    if(!response.ok){
+        throw new Error(
+            `Failed to fetch countries. Status: ${response.status} ${response.statusText}`
+        )
+    }
+    const countries = await response.json();
+
 }
 
 main(); 
