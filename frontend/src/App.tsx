f5bdc91e9143baf0f6ea289a3a97f200f48ac257
Markus Olsen <markusol@hiof.no>
2026-09-15 17:53:24 +0200

    Started to code logic for LoginPage
    +Created fundamental logic for LoginPage for the coming /Feature/LoginPage branch
    +New route for LoginPage in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,11 +4,13 @@ import CountryDetailPage from "./pages/CountryDetailPage";
 import CountryListPage from "./pages/CountryListPage";
 import { Routes, Route } from "react-router";
 import MainLayout from "../src/Layout/MainLayout";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <>
       <Routes>
+        <Route path="/" element={<LoginPage />} />
         <Route element={<MainLayout />}>
           <Route path="/countries" element={<CountryListPage />} />
           <Route
---
+++ frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,5 @@
+function LoginPage() {
+  return <button>Demo login</button>;
+}
+
+export default LoginPage;
