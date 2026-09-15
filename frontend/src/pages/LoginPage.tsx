afc0726f762380cadc4b7841f0295e56ad25c6fe
Markus Olsen <markusol@hiof.no>
2026-09-16 00:16:47 +0200

    Styling and added login button for demo account in LoginPage.tsx
    + Added button for login to the demo account
    + Added text (information about the app) in LoginPage.tsx
    + Added a button for nagigation to CountryListPage.tsx to get an overview over all countries
    + Created stylesheet for LoginPage.tsx (LoginPage.css) with some temporary design
    + Added photo for login page in the asset folder

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router";
+import loginPagePhoto from "../assets/loginPage-photo.png";
+import "./LoginPage.css";
 
 function LoginPage() {
   async function clickDemo() {
@@ -25,9 +27,30 @@ function LoginPage() {
 
   return (
     <>
-      <Link to={"/countries"} onClick={clickDemo}>
-        Demo login
-      </Link>
+      <div>
+        <section className="main-section">
+          <article>
+            <h1>Utforsk verden.</h1>
+            <h2>Planlegg dine eventyr!</h2>
+            <p>
+              Country Explorer hjelper deg med å holde oversikt over land du
+              ønsker å besøke, planlegger å besøke eller allerede har besøkt.
+            </p>
+            <Link
+              to={"/countries"}
+              className={"demo-login-button"}
+              onClick={clickDemo}
+            >
+              Demo login
+            </Link>
+            <Link to={"/countries"} className="show-countries-button">
+              Se Land
+            </Link>
+            <p>Om appen</p>
+          </article>
+          <img src={loginPagePhoto} alt="Reisende ved tog i fjellandskap" />
+        </section>
+      </div>
     </>
   );
 }
