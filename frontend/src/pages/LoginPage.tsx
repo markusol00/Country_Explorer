4488f737d4068f8e9334765e0cd1e198e3a5d153
Markus Olsen <markusol@hiof.no>
2026-09-15 19:27:33 +0200

    Created button for login to the demo user
    +Created logic in LoginPage.tsx for logging into the demo-users account

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,5 +1,35 @@
+import { Link } from "react-router";
+
 function LoginPage() {
-  return <button>Demo login</button>;
+  async function clickDemo() {
+    //returns a response object
+    try {
+      const response = await fetch("http://localhost:3000/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          //Hardcoded values for the demo-user (not a real mail)
+          email: "demo@countryexplorer.com",
+          password: "demo123",
+        }),
+      });
+      console.log(response);
+      console.log("Status:", response.status);
+      console.log("OK:", response.ok);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  return (
+    <>
+      <Link to={"/countries"} onClick={clickDemo}>
+        Demo login
+      </Link>
+    </>
+  );
 }
 
 export default LoginPage;
